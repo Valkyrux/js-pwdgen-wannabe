2378773c574fa97c5ab68e461a8b47027c41286c
Valkyrux <philadephos.granata@gmail.com>
2021-11-18 15:21:18 +0100

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+// assegnazione da input alla variabile firstName il nome dell'utente 
+let userName = prompt('Inserisci il tuo nome');
+// console.log(firstName)
+
+let userSurname = prompt('Inserisci il tuo cognome');
+// console.log(userSurname);
